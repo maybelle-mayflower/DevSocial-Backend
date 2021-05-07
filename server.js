a90b19939c1f3c476fe59ed9b78fc73cd138dd2a
maybelle <maybellekakulu@gmail.com>
2021-05-07 10:15:47 +0100

    ready

--- server.js
+++ server.js
@@ -19,14 +19,15 @@ app.use('/api/profile', require('./routes/api/profile'));
 
 
 //Serve static assets in production
-if(process.env.NODE_ENV === 'production'){
+//if(process.env.NODE_ENV === 'production'){
 
     //Set static folder
     app.use(express.static('client/build'));
+
     app.get('*', (req, res) => {
-        return res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.join(__dirname+'/client/build/index.html'));
     });
-}
+//}
 
 const PORT = process.env.PORT || 5000; 
 
