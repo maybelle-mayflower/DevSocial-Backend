93538cd1e2f3cdc4932e83fe516f98b93bcdc1bf
maybelle <maybellekakulu@gmail.com>
2021-05-07 10:22:21 +0100

    add cli folder

--- server.js
+++ server.js
@@ -19,15 +19,15 @@ app.use('/api/profile', require('./routes/api/profile'));
 
 
 //Serve static assets in production
-//if(process.env.NODE_ENV === 'production'){
+if(process.env.NODE_ENV === 'production'){
 
     //Set static folder
     app.use(express.static('client/build'));
-
+    
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname+'/client/build/index.html'));
     });
-//}
+}
 
 const PORT = process.env.PORT || 5000; 
 
